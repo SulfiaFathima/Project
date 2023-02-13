ac9cc98625fd2df6eb5cfc0d7112ae6b1f6a8c19
SulfiaFathima <114172240+SulfiaM@users.noreply.github.com>
2023-02-13 15:22:51 +0530

    test commit

---
+++ feb-13-2023/app.js
@@ -0,0 +1,10 @@
+localStorage.setItem('name','ajmal');
+console.log(localStorage.getItem('name'));
+// localStorage.removeItem('name');
+sessionStorage.setItem('name', 'nazi');
+console.log(sessionStorage.getItem('name'));
+sessionStorage.setItem('name', 'rabeek');
+console.log(sessionStorage.getItem('name'));
+// sessionStorage.removeItem('name');
+document.cookie = 'name=sulfia; expires='+new Date(2023, 10, 18).toUTCString();
+document.cookie = 'lastname=fathima; expires='+new Date(2023, 10, 18).toUTCString();
